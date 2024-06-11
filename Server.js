7a9b22df94cbaf69a11bf2b4cd0e55e8b54a9101
DYuHLin <damianver2000@gmail.com>
2024-06-11 16:30:12 +0200

    Increased the payload.

--- Server.js
+++ Server.js
@@ -17,11 +17,11 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(session({secret: "cats", resave: false, saveUninitialized: true}));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 
 mongoose.set('strictQuery', false);
 const mongoDb = process.env.MONGODB_URL;
